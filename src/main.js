9e147859b86fb49f7e66ee131bffdd7daf342b76
huychongxang <huychongxang@gmail.com>
2019-11-27 20:46:27 +0700

    a

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import DummyButton from './components/DummyButton.vue';
+export default DummyButton;
 export default {
-  install (Vue, options) {
+  install (Vue) {
     Vue.component('dummy-button', DummyButton)
   }
 }
