0a3b20cc0e194951f9c935d5850f1dead534793b
huychongxang <huychongxang@gmail.com>
2019-11-27 17:03:23 +0700

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import DummyButton from './plugin.js';
 Vue.config.productionTip = false
 
+
+
+Vue.use(DummyButton);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
---
+++ src/plugin.js
@@ -0,0 +1,6 @@
+import DummyButton from './components/DummyButton.vue';
+export default {
+    install(Vue) {
+        Vue.component("dummy-button", DummyButton);
+    },
+};
