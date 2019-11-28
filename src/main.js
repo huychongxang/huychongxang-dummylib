2141bf5ad77fc03a553039aa62cb1cbc69b141e1
huychongxang <huychongxang@gmail.com>
2019-11-28 15:43:08 +0700

    v1.0.13

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import store from './store';
 import App from './App.vue';
 import DummyButton from './plugin.js';
-Vue.use(DummyButton);
+Vue.use(DummyButton,{
+    store
+});
 
 new Vue({
     el:'#app',
