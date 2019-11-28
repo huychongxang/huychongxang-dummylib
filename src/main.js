e7655395a546004f5b0f45e62381947f87362cb2
huychongxang <huychongxang@gmail.com>
2019-11-28 15:32:34 +0700

    test toi uu

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
 import store from './store';
 import App from './App.vue';
 import DummyButton from './plugin.js';
-
-Vue.use(Vuex);
-Vue.use(DummyButton,{
-    store
-});
+Vue.use(DummyButton);
 
 new Vue({
     el:'#app',
